88f80439f580dca91ca5e7438291892785bcb4da
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 18:24:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,9 +17,9 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+  #  name.downcase.gsub(' ', '-')
+  #end
 
   def self.all
     @@songs
