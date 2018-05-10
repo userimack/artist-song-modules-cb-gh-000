08619985e7e75e88006fde05e7ef44b41c995165
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 18:20:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
   
-  extend Findable
+  extend Findable::ClassMethods
   
   attr_accessor :name
   attr_reader :songs
