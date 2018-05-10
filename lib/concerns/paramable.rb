6a974198c3a46553fed4eab33c1038d1f0cee99b
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 18:24:28 +0000

    automatically backed up by learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -1,6 +1,7 @@
 module Paramable 
   module InstanceMethods
-  def to_param
-    self.class.name.downcase.gsub(' ', '-')
+    def to_param
+      self.class.name.downcase.gsub(' ', '-')
+    end
   end
 end 
